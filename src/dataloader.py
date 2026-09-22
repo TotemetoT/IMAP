985f0971d52a8e22df794baac70aa5348f0ae584
TotemetoT <mschatz360@gmail.com>
2026-09-21 22:18:24 -0400

    dataloader implementation

---
+++ src/dataloader.py
@@ -0,0 +1,30 @@
+import csv
+
+import configs as cfg
+
+class Course:
+    def __init__(self, row):
+        self.row = row
+        self.CRN = row[0]
+        self.course = row[1]
+        self.title = row[3]
+        self.days = row[7]
+        self.time = row[8]
+        self.location = row[9]
+        self.instructor = row[10]
+
+def Load_CSV():
+    """
+    Loads the CSV without courses that are not in person:
+    ARR, HYB, ASY, etc. 
+    
+    @return: List of rows of SYN classes"""
+    fname = cfg.COURSE_PATH
+    rows = []
+    with open(fname, "r") as f:
+        reader = csv.reader(f)
+        next(reader) 
+        for row in reader:
+            if row[9] != " " and row[9] != "HYB " and row[9] != "ASY " and row[9] != "ARR ":
+                rows.append(Course(row))
+    return rows
--- src/filter.py
+++ src/filter.py
@@ -1,5 +1,6 @@
 import csv
 
+from dataloader import Load_CSV
 import configs as cfg
 import utils as u
 
@@ -10,51 +11,25 @@ def search_by():
         print(f"{i}: {filter}")
         i += 1
 
-def Load_CSV():
-    """
-    Loads the CSV without courses that are not in person:
-    ARR, HYB, ASY, etc. 
-    
-    @return: List of rows of SYN classes"""
-    fname = cfg.COURSE_PATH
-    rows = []
-    with open(fname, "r") as f:
-        reader = csv.reader(f)
-        next(reader) 
-        for row in reader:
-            if row[9] != " " and row[9] != "HYB " and row[9] != "ASY " and row[9] != "ARR ":
-                rows.append(row)
-    return rows
-            
-
 def CRN(val):
-    fname = cfg.FALL_2026_PATH
-    with open(fname, "r") as f:
-        reader = csv.reader(f)
-        next(reader) 
-        for row in reader:
-            if row[0] == str(val):
+        for row in Load_CSV():
+            if row.CRN == str(val):
                 return row
         return None
 #TODO Refactor for Load_CSV()
 def Location(val):
-    fname = cfg.COURSE_PATH
     L_dict = {}
-    with open(fname, 'r') as f:
-        reader = csv.reader(f)
-        next(reader)
-        for row in reader: 
-            if row[9].lower()[:len(val)] == val.lower():
-                if row[9][-5].isnumeric():
-                    if row[9][-4:] == " ": v = 3
-                    else: v = 5
-                elif row[9][-4] == " ": v = 3
-                else: v = 4
-                print(v, row[9][-v:])
-                if row[9][-v:] not in L_dict:
-                    L_dict[row[9][-v:]] = 1
-                else:
-                    L_dict[row[9][-v:]] += 1                
+    for row in Load_CSV(): 
+        if (row.location).lower()[:len(val)] == val.lower():
+            if (row.location)[-5].isnumeric():
+                if (row.location)[-4:] == " ": v = 3
+                else: v = 5
+            elif (row.location)[-4] == " ": v = 3
+            else: v = 4
+            if (row.location)[-v:] not in L_dict:
+                L_dict[(row.location)[-v:]] = 1
+            else:
+                L_dict[(row.location)[-v:]] += 1                
     return L_dict
 
 def Time(t, d):
@@ -65,9 +40,9 @@ def Time(t, d):
     all_rooms = set()
 
     for course in courses:
-        location = course[9]
-        days = course[7]
-        times = course[8]
+        location = course.location
+        days = course.days
+        times = course.time
 
         locations = [x.strip() for x in location.split(";")]
         time_ranges = [x.strip() for x in times.split(";")]
@@ -90,7 +65,7 @@ def Time(t, d):
         # If the course is active, its rooms are being used
         if course_is_active:
             for room in locations:
-                used.setdefault(room, []).append(course[1])
+                used.setdefault(room, []).append(course.CRN)
 
     # Anything not used is unused
     unused = {}
@@ -122,17 +97,4 @@ def Time(t, d):
     return u.clean_dict(used_rooms), u.clean_dict(unused_rooms)
 
 if __name__ == "__main__":
-    dicts = Time("9000", "M")
-    i = 0
-    l = ["used", "unused"]
-    for dict in dicts:
-        print("======================")
-        print(l[i])
-        i += 1
-        print("======================")
-        for key in dict:
-            print(f"{key}: {dict[key]}")
-        print('\n')
-
-
-    # print(Location("FERG"))
+    print(Time(1100, "M"))
